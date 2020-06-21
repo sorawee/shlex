6e450fb07283022e6e3aeba8867c285a591d5346
Sorawee Porncharoenwase <sorawee.pwase@gmail.com>
2020-06-21 10:24:20 -0700

    Add a missing dependency

--- info.rkt
+++ info.rkt
@@ -1,6 +1,7 @@
 #lang info
 (define collection "shlex")
-(define deps '("base"))
+(define deps '("parser-tools-lib"
+               "base"))
 (define build-deps '("scribble-lib" "racket-doc" "rackunit-lib"))
 (define scribblings '(("scribblings/shlex.scrbl" ())))
 (define pkg-desc "shlex for Racket: Simple lexical analysis")
